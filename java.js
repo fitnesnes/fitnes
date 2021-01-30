34818cd560cbf3e3d6150009c100c67ad9d74c19
fitnesnes <77645329+fitnesnes@users.noreply.github.com>
2021-01-30 22:05:34 +0100

    Add files via upload

--- java.js
+++ java.js
@@ -1,5 +1,27 @@
-         <SCRIPT LANGUAGE="JavaScript">  
-        			function evaluation()  {    
-						document.formulaire.calcul.value=eval(document.formulaire.saisie.value);  
-			}
-		</SCRIPT>
+function requiredElement() {
+    var nom = document.getElementById("nom").value;
+    var prenom = document.getElementById("prenom").value;
+    var email = document.getElementById("email").value;
+    
+    if(nom== "") {
+    alert("Veillez insérer votre Nom");
+    document.getElementById("nom").style.backgroundColor="red";
+    document.getElementById("nom").focus();
+    //nom.focus();
+    return false;
+    }
+    if(prenom == "") {
+    alert("Veillez insérer votre Prénom");
+    document.getElementById("prenom").style.backgroundColor="red";
+    document.getElementById("prenom").focus();
+    return false;
+    }
+    if(email == "") {
+    alert("Veillez insérer votre Email");
+    document.getElementById("email").style.backgroundColor="red";
+    document.getElementById("email").focus();
+    return false;
+    }
+    return true;
+    }
+    
