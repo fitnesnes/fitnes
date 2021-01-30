581f7378901a34407c6329ce0de1ac534ef5d211
fitnesnes <77645329+fitnesnes@users.noreply.github.com>
2021-01-30 22:01:34 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,5 @@
+         <SCRIPT LANGUAGE="JavaScript">  
+        			function evaluation()  {    
+						document.formulaire.calcul.value=eval(document.formulaire.saisie.value);  
+			}
+		</SCRIPT>
